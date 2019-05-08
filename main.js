f451212cea99d431530cd8220ad75d727af0447c
David Guan <davidguandev@gmail.com>
2019-05-08 22:11:55 +1000

    Validate user data

--- main.js
+++ main.js
@@ -5,62 +5,57 @@ const { PersistentStore } = require("./src/data-store");
 const { googleApiClient } = require("./src/google-api");
 const { matchContest } = require("./src/helpers");
 const { Logger } = require("./src/logger");
+const { getUsers } = require("./src/user");
 
 const { CLIST_KEY, SENTRY_ID } = process.env;
-if (SENTRY_ID) {
-  const sentryDsn = `https://${SENTRY_ID}@sentry.io/1443501`;
-  sentry.init({ dsn: sentryDsn });
-}
-const logger = new Logger(SENTRY_ID ? sentry : undefined);
-googleApiClient.init();
 
-let users = [];
-const usersStore = new PersistentStore("users");
-usersStore.get().then(ret => {
-  users = ret;
-});
-
-let events = [];
-let eventsLoaded = false;
-const eventsStore = new PersistentStore("events");
-eventsStore.get().then(ret => {
-  eventsLoaded = true;
-  events = ret;
-});
-
-// An Observer who does the main work.
-const contestsObserver = {
-  update: ret => {
-    if (!eventsLoaded) return;
-    users.forEach(u => {
-      const relatedEvents = events.filter(d => d.userEmail == u.email);
-      ret
-        .filter(d => matchContest(d, u))
-        .forEach(d => {
-          if (relatedEvents.find(e => e.contestId == d.id)) {
-            console.log(`Already invited ${u.email} to ${d.event} :)`);
-            return;
-          }
-          googleApiClient
-            .invite({
-              name: d.event,
-              start: d.start,
-              end: d.end,
-              eventLink: d.href,
-              email: u.email
-            })
-            .then(() => {
-              events.push({
-                userEmail: u.email,
-                contestId: d.id
-              });
-              eventsStore.write(events);
-            });
-        });
-    });
+async function run() {
+  if (SENTRY_ID) {
+    const sentryDsn = `https://${SENTRY_ID}@sentry.io/1443501`;
+    sentry.init({ dsn: sentryDsn });
   }
-};
+  const logger = new Logger(SENTRY_ID ? sentry : undefined);
+  const clistClient = new ClistClient(CLIST_KEY, logger);
+  googleApiClient.init();
 
-const clistClient = new ClistClient(CLIST_KEY, logger);
-clistClient.registerObserver(contestsObserver);
-clistClient.startQuery(50000);
+  const users = await getUsers();
+  const eventsStore = new PersistentStore("events");
+  const events = await eventsStore.get();
+
+  // An Observer who does the main work.
+  const contestsObserver = {
+    update: ret => {
+      users.forEach(u => {
+        const relatedEvents = events.filter(d => d.userEmail == u.email);
+        ret
+          .filter(d => matchContest(d, u))
+          .forEach(d => {
+            if (relatedEvents.find(e => e.contestId == d.id)) {
+              console.log(`Already invited ${u.email} to ${d.event} :)`);
+              return;
+            }
+            googleApiClient
+              .invite({
+                name: d.event,
+                start: d.start,
+                end: d.end,
+                eventLink: d.href,
+                email: u.email
+              })
+              .then(() => {
+                events.push({
+                  userEmail: u.email,
+                  contestId: d.id
+                });
+                eventsStore.write(events);
+              });
+          });
+      });
+    }
+  };
+
+  clistClient.registerObserver(contestsObserver);
+  clistClient.startQuery(50000);
+}
+
+run();
